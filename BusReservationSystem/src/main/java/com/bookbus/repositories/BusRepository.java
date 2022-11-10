4470c26f86e14d2f6451dcebdd9b06c4dbd99c94
Yash Rajput <105929413+rajputyash006@users.noreply.github.com>
2022-11-11 03:21:36 +0530

    bus repository jpa done

---
+++ BusReservationSystem/src/main/java/com/bookbus/repositories/BusRepository.java
@@ -0,0 +1,18 @@
+package com.bookbus.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.bookbus.models.Bus;
+
+@Repository
+public interface BusRepository extends JpaRepository<Bus,Integer>{
+	
+	public List<Bus> FindBybusType(String busType);
+	
+	@Query("from Bus")
+	public List<Bus> viewAllBus();
+}
