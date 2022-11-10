83aa4612db0cadd14b3288c3669919b2789841b9
BhaveshShahani <gudd.shahani@gmail.com>
2022-11-11 02:26:14 +0530

    fw19_0680_day-3

---
+++ BusReservationSystem/src/main/java/com/bookbus/exceptions/FeedbackException.java
@@ -0,0 +1,16 @@
+package com.bookbus.exceptions;
+
+public class FeedbackException extends Exception {
+
+	public FeedbackException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FeedbackException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
--- BusReservationSystem/src/main/java/com/bookbus/services/FeedbackService.java
+++ BusReservationSystem/src/main/java/com/bookbus/services/FeedbackService.java
@@ -2,6 +2,7 @@ package com.bookbus.services;
 
 import java.util.List;
 
+import com.bookbus.exceptions.FeedbackException;
 import com.bookbus.models.Feedback;
 
 
@@ -9,9 +10,9 @@ public interface FeedbackService {
 
 	public Feedback addFeedBack(Feedback feedback);
 	
-	public Feedback updateFeedBack (Feedback feedback);
+	public Feedback updateFeedBack (Feedback feedback) throws FeedbackException;
 	
-	public Feedback viewFeedBack(int feedbackid);
+	public Feedback viewFeedBack(int feedbackid) throws FeedbackException;
 	
-	public List<Feedback> viewAllFeedBack();
+	public List<Feedback> viewAllFeedBack() throws FeedbackException;
 }
