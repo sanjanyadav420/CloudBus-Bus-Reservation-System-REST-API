87fe6b7afc33f76c9ea967ad084bc15415594066
Yash Rajput <105929413+rajputyash006@users.noreply.github.com>
2022-11-11 14:28:03 +0530

    Update LogException.java

--- BusReservationSystem/src/main/java/com/bookbus/exceptions/LogException.java
+++ BusReservationSystem/src/main/java/com/bookbus/exceptions/LogException.java
@@ -12,18 +12,3 @@ public class LogException extends Exception{
 	}
 
 }
-
-package com.bookbus.exceptions;
-
-public class LogException extends Exception{
-	
-	public LogException() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public LogException(String message) {
-		super(message);
-	}
-
-}
-
