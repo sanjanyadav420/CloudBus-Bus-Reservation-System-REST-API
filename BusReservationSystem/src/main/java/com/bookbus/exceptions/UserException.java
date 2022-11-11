3fe112d9791db56e4d8f287295276357ef9d7346
Yash Rajput <105929413+rajputyash006@users.noreply.github.com>
2022-11-11 14:35:21 +0530

    Update UserException.java

--- BusReservationSystem/src/main/java/com/bookbus/exceptions/UserException.java
+++ BusReservationSystem/src/main/java/com/bookbus/exceptions/UserException.java
@@ -12,18 +12,4 @@ public class UserException extends Exception{
 	}
 
 }
-=======
-package com.bookbus.exceptions;
-
-public class UserException extends Exception{
-	
-	public UserException() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public UserException(String message) {
-		super(message);
-	}
-
-}
 
