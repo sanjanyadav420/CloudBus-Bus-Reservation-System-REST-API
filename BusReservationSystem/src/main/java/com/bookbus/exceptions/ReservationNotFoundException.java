403d3761f82ff52c453a76c1df7feca9b9fa6ac1
Yash Rajput <105929413+rajputyash006@users.noreply.github.com>
2022-11-11 14:32:07 +0530

    Update ReservationNotFoundException.java

--- BusReservationSystem/src/main/java/com/bookbus/exceptions/ReservationNotFoundException.java
+++ BusReservationSystem/src/main/java/com/bookbus/exceptions/ReservationNotFoundException.java
@@ -11,17 +11,3 @@ public class ReservationNotFoundException extends Exception{
 		super(message);
 	}
 }
-=======
-package com.bookbus.exceptions;
-
-public class ReservationNotFoundException extends Exception{
-
-	public ReservationNotFoundException() {
-		
-	}
-	
-	public ReservationNotFoundException(String message) {
-		super(message);
-	}
-}
-
