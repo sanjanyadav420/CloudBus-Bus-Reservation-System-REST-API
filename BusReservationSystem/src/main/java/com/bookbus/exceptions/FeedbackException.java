5a5680d564236532fdc98c9072cb230c49fadb69
Yash Rajput <105929413+rajputyash006@users.noreply.github.com>
2022-11-11 14:26:52 +0530

    Update FeedbackException.java

--- BusReservationSystem/src/main/java/com/bookbus/exceptions/FeedbackException.java
+++ BusReservationSystem/src/main/java/com/bookbus/exceptions/FeedbackException.java
@@ -15,21 +15,3 @@ public class FeedbackException extends Exception {
 
 	
 }
-=======
-package com.bookbus.exceptions;
-
-public class FeedbackException extends Exception {
-
-	public FeedbackException() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public FeedbackException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-
-	
-}
-
