44e3e2f81ebe9a0e0172c57f04512bf55e0f1993
Yash Rajput <105929413+rajputyash006@users.noreply.github.com>
2022-11-11 14:30:00 +0530

    Update MyErrorDetails.java

--- BusReservationSystem/src/main/java/com/bookbus/exceptions/MyErrorDetails.java
+++ BusReservationSystem/src/main/java/com/bookbus/exceptions/MyErrorDetails.java
@@ -19,25 +19,3 @@ public class MyErrorDetails {
 	
 	
 }
-
-package com.bookbus.exceptions;
-
-import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-public class MyErrorDetails {
-
-	private LocalDateTime timestamp;
-	private String message;
-	private String details;
-	
-	
-}
-
