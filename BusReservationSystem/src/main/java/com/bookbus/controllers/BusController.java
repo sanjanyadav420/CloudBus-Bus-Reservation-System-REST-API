a3c6c58c0c60603e092a1dd17d363f750e075e78
Yash Rajput <105929413+rajputyash006@users.noreply.github.com>
2022-11-13 01:02:07 +0530

    Add files via upload
    changes done

--- BusReservationSystem/src/main/java/com/bookbus/controllers/BusController.java
+++ BusReservationSystem/src/main/java/com/bookbus/controllers/BusController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bookbus.dto.BusDto;
@@ -21,7 +20,6 @@ import com.bookbus.models.Bus;
 import com.bookbus.services.BusService;
 
 @RestController
-@RequestMapping("/cloudbus")
 public class BusController {
 	
 	@Autowired
