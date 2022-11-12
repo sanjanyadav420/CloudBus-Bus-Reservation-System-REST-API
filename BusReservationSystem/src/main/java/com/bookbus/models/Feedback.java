9c0527c3af18240b7191c607a59b16b06eef4e0f
Mukund Jha <mukundjha1310@gmail.com>
2022-11-12 18:49:50 +0530

    fw19_0680

--- BusReservationSystem/src/main/java/com/bookbus/models/Feedback.java
+++ BusReservationSystem/src/main/java/com/bookbus/models/Feedback.java
@@ -1,18 +1,13 @@
 package com.bookbus.models;
 
 import java.time.LocalDate;
-
-import javax.annotation.Generated;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Size;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -27,26 +22,28 @@ public class Feedback {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer feedBackId;
 	
-	@Min(0)
-	@Max(value= 5, message = "Rate Between 0 to 5")
+//	@Min(0)
+//	@Max(value= 5, message = "Rate Between 0 to 5")
 	private Integer driverRating;
 	
-	@Min(0)
-	@Max(value= 5, message = "Rate Between 0 to 5")
+//	@Min(0)
+//	@Max(value= 5, message = "Rate Between 0 to 5")
 	private Integer serviceRating;
 	
-	@Min(0)
-	@Max(value= 5, message = "Rate Between 0 to 5")
+//	@Min(0)
+//	@Max(value= 5, message = "Rate Between 0 to 5")
 	private Integer overallRating;
 	
-	@Size(min = 5, max = 25, message = "Minimun 5 letters")
+//	@Size(min = 5, max = 25, message = "Minimun 5 letters")
 	private String comments;
 	
 	private LocalDate feedbackdate;
 	
+	@JsonIgnore
 	@OneToOne(cascade =CascadeType.ALL)
 	private User users;
 	
+	@JsonIgnore
 	@OneToOne(cascade =CascadeType.ALL)
 	private Bus bus;
 
--- BusReservationSystem/src/main/java/com/bookbus/serviceImpl/FeedbackServiceImpl.java
+++ BusReservationSystem/src/main/java/com/bookbus/serviceImpl/FeedbackServiceImpl.java
@@ -2,13 +2,15 @@ package com.bookbus.serviceImpl;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.bookbus.exceptions.FeedbackException;
+import com.bookbus.exceptions.LogException;
+import com.bookbus.models.CurrentUserSession;
 import com.bookbus.models.Feedback;
 import com.bookbus.repositories.FeedbackRepo;
+import com.bookbus.repositories.UserLogRepo;
+import com.bookbus.repositories.UserRepo;
 import com.bookbus.services.FeedbackService;
 
 @Service
@@ -17,28 +19,50 @@ public class FeedbackServiceImpl implements FeedbackService {
 	@Autowired
 	private FeedbackRepo feedbackRepo;
 
+	@Autowired
+	private UserLogRepo userlogrepo;
+
+	@Autowired
+	private UserRepo userRepo;
+
 	@Override
-	public Feedback addFeedBack(Feedback feedback) {
+	public Feedback addFeedBack(Feedback feedback, Integer UserId) throws LogException {
 
-		return feedbackRepo.save(feedback);
-	}
+		Optional<CurrentUserSession> cus = userlogrepo.findById(UserId);
 
-	@Override
-	public Feedback updateFeedBack(Feedback feedback) throws FeedbackException {
-		Optional<Feedback> updateFeedback = feedbackRepo.findById(feedback.getFeedBackId());
-
-		if (updateFeedback.isEmpty()) {
-
-			throw new FeedbackException("Feedback not found");
+		if (cus.isPresent()) {
+			return feedbackRepo.save(feedback);
+		} else {
+			throw new LogException("Please Login First/ Inavlid User Id " + UserId);
 		}
-		Feedback updatedFeedBack = feedbackRepo.save(updateFeedback.get());
-
-		return updatedFeedBack;
 
 	}
 
 	@Override
-	public Feedback viewFeedBack(int feedbackid) throws FeedbackException {
+	public Feedback updateFeedBack(Feedback feedback, Integer UserId) throws FeedbackException, LogException {
+
+		Optional<CurrentUserSession> cus = userlogrepo.findById(UserId);
+
+		if (cus.isPresent()) {
+			Optional<Feedback> updateFeedback = feedbackRepo.findById(feedback.getFeedBackId());
+
+			if (updateFeedback.isEmpty()) {
+
+				throw new FeedbackException("Feedback not found");
+			} else {
+				Feedback updatedFeedBack = feedbackRepo.save(updateFeedback.get());
+
+				return updatedFeedBack;
+			}
+
+		} else {
+			throw new LogException("Please Login First/ Inavlid User Id " + UserId);
+		}
+
+	}
+
+	@Override
+	public Feedback viewFeedBack(Integer feedbackid) throws FeedbackException {
 		return feedbackRepo.findById(feedbackid)
 				.orElseThrow(() -> new FeedbackException("FeedBack with Id " + feedbackid + " not found"));
 	}
